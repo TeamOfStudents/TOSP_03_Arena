e93d8c80c5e5fc56d9b36a12fb6c4a237670f9d2
Holger <86824184+HolgerSin@users.noreply.github.com>
2021-11-09 11:24:07 +0100

    initial sample Maven project

---
+++ src/main/java/com/github/teamofstudents/tosp_03_arena/App.java
@@ -0,0 +1,13 @@
+package com.github.teamofstudents.tosp_03_arena;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/github/teamofstudents/tosp_03_arena/AppTest.java
@@ -0,0 +1,20 @@
+package com.github.teamofstudents.tosp_03_arena;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
